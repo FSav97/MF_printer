06276ba55e681d6667ac38014b5e9548d7941568
Francesco Savona <fsav97@gmail.com>
2025-05-01 09:53:58 +0200

    Updated X_MAX_POS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1888,7 +1888,7 @@
 #define X_MIN_POS -1
 #define Y_MIN_POS -1
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + 10
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 350
 //#define I_MIN_POS 0
